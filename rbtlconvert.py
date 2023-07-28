d7c9a240b40c642727aedc51a818edbb1fa8949e
Andrew Potter <apotter1@una.edu>
2023-07-28 13:59:03 -0500

    Add files via upload
    Added code for converting RBTL numeric identifiers to alpha

---
+++ rbtlconvert.py
@@ -0,0 +1,33 @@
+# convert RBTL numeric identifiers to alpha
+
+# test examples
+#exp = '((((((2 → 3) ∧ 2) → 3) → 1) ∧ (((2 → 3) ∧ 2) → 3)) → 1)'
+exp = '(((((((((3 → 2) ∧ 3) → 2) → 1) ∧ (((3 → 2) ∧ 3) → 2)) → 1) → ((((((¬(5 → ¬(((7 ∨ 6) ∧ ¬7) → 6)) → (((7 ∨ 6) ∧ ¬7) → 6)) ∧ ¬(5 → ¬(((7 ∨ 6) ∧ ¬7) → 6))) → (((7 ∨ 6) ∧ ¬7) → 6)) → 4) ∧ (((¬(5 → ¬(((7 ∨ 6) ∧ ¬7) → 6)) → (((7 ∨ 6) ∧ ¬7) → 6)) ∧ ¬(5 → ¬(((7 ∨ 6) ∧ ¬7) → 6))) → (((7 ∨ 6) ∧ ¬7) → 6))) → 4)) ∧ ((((((3 → 2) ∧ 3) → 2) → 1) ∧ (((3 → 2) ∧ 3) → 2)) → 1)) → ((((((¬(5 → ¬(((7 ∨ 6) ∧ ¬7) → 6)) → (((7 ∨ 6) ∧ ¬7) → 6)) ∧ ¬(5 → ¬(((7 ∨ 6) ∧ ¬7) → 6))) → (((7 ∨ 6) ∧ ¬7) → 6)) → 4) ∧ (((¬(5 → ¬(((7 ∨ 6) ∧ ¬7) → 6)) → (((7 ∨ 6) ∧ ¬7) → 6)) ∧ ¬(5 → ¬(((7 ∨ 6) ∧ ¬7) → 6))) → (((7 ∨ 6) ∧ ¬7) → 6))) → 4))'
+
+numlist = []
+abc = []
+c = 'a'
+
+# initialize parallel lists
+
+for i in range(1,27):
+    numlist.append(i)
+    abc.append(c)
+    c = chr(ord(c) + 1)
+
+# reverse order required to handle > 9 digits
+
+numlist.reverse()
+abc.reverse()
+print(exp)
+print()
+
+# replace digits with letters
+
+for i in range(0,26):
+    exp = exp.replace(str(numlist[i]),abc[i])
+    i += 1    
+
+print(exp)
+print()
+print(exp.upper())
